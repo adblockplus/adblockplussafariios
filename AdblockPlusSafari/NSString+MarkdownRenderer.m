e822290b6900aa01c2aed2d0e281701879e6661c
Jan Dedecek <jan@kitt.com>
2015-10-30 15:37:50 +0100

    Added Eyeo's licence headers.

--- AdblockPlusSafari/NSString+MarkdownRenderer.m
+++ AdblockPlusSafari/NSString+MarkdownRenderer.m
@@ -1,10 +1,19 @@
-//
-//  NSString+MarkdownRenderer.m
-//  AdblockPlusSafari
-//
-//  Created by Pavel Zdeněk on 2.O.15.
-//  Copyright © 2015 Eyeo GmbH. All rights reserved.
-//
+/*
+ * This file is part of Adblock Plus <https://adblockplus.org/>,
+ * Copyright (C) 2006-2015 Eyeo GmbH
+ *
+ * Adblock Plus is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * Adblock Plus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Adblock Plus.  If not, see <http://www.gnu.org/licenses/&gt.
+ */
 
 #import "NSString+MarkdownRenderer.h"
 @import UIKit;
