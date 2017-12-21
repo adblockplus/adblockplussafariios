5fe821cc784a15de7f4c245e5bbda266b62ac421
Daniel Zhang <d.zhang@adblockplus.org>
2017-12-21 11:26:44 -1000

    Started adding code for dl delegate

--- AdblockPlusSafari/FilterListsUpdater+DownloadDelegate.swift
+++ AdblockPlusSafari/FilterListsUpdater+DownloadDelegate.swift
@@ -1,10 +1,23 @@
+/// Implementation of URLSessionDownloadDelegate.
 extension FilterListsUpdater {
-    // ------------------------------------------------------------
-    // MARK: - URLSessionDownloadDelegate -
-    // ------------------------------------------------------------
+    func urlSession(_ session: URLSession,
+                    task: URLSessionTask,
+                    didCompleteWithError error: Error?)
+    {
+
+    }
 
     func urlSession(_ session: URLSession,
                     downloadTask: URLSessionDownloadTask,
-                    didFinishDownloadingTo location: URL) {
+                    didFinishDownloadingTo location: URL)
+    {
+    }
+
+    /// Return the filter list name for a given task identifier.
+    func filterListNameForTaskTaskIdentifier(taskIdentifier: Int) -> FilterListName
+    {
+        for name in filterLists {
+
+        }
     }
 }
