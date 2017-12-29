b0f6833886552b93977ce27abfa737c78734e79c
Daniel Zhang <d.zhang@adblockplus.org>
2017-12-28 14:31:48 -1000

    Started delegate methods

--- AdblockPlusSafari/FilterListsUpdater+DownloadDelegate.swift
+++ AdblockPlusSafari/FilterListsUpdater+DownloadDelegate.swift
@@ -1,21 +1,37 @@
 /// Implementation of URLSessionDownloadDelegate.
 extension FilterListsUpdater {
-    func urlSession(_ session: URLSession,
-                    task: URLSessionTask,
-                    didCompleteWithError error: Error?)
-    {
+//    func urlSession(_ session: URLSession,
+//                    task: URLSessionTask,
+//                    didCompleteWithError error: Error?)
+//    {
+//        dLog("", date: "2017-Dec-28")
+//    }
 
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?)
+    {
+        dLog("", date: "2017-Dec-28")
     }
 
-    func urlSession(_ session: URLSession,
-                    downloadTask: URLSessionDownloadTask,
-                    didFinishDownloadingTo location: URL)
+//    func urlSession(_ session: URLSession,
+//                    downloadTask: URLSessionDownloadTask,
+//                    didFinishDownloadingTo location: URL)
+//    {
+//        dLog("", date: "2017-Dec-28")
+//    }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL)
     {
+        dLog("", date: "2017-Dec-28")
+    }
+
+    func fileManager(_ fileManager: FileManager, shouldProceedAfterError error: Error, removingItemAt URL: URL) -> Bool {
+        return true
     }
 
     /// Return the filter list name for a given task identifier.
     func filterListNameForTaskTaskIdentifier(taskIdentifier: Int) -> FilterListName
     {
+        dLog("", date: "2017-Dec-28")
         for name in filterLists {
 
         }
